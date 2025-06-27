21ed955d8468116686f0a35173a7f44cf8302953
Muyun2023 <ji.mu@northeastern.edu>
2025-06-27 18:07:04 +0800

    Update Problem.py

--- Session 1: Strings & Arrays/Advanced Problem Set Version/Advanced Problem Set Version 2/Problem.py
+++ Session 1: Strings & Arrays/Advanced Problem Set Version/Advanced Problem Set Version 2/Problem.py
@@ -26,3 +26,39 @@ print(final_value_after_operations(operations))
 
 operations = ["bouncy", "bouncy", "flouncy"]
 print(final_value_after_operations(operations))
+
+def tiggerfy(word):
+    word = word.lower()
+    for sb in ['t','i','gg','er']:
+        word = word.replace(sb,'')
+    return word
+
+word = "Trigger"
+print(tiggerfy(word))
+
+word = "eggplant"
+print(tiggerfy(word))
+
+word = "Choir"
+print(tiggerfy(word))
+
+def non_decreasing(nums):
+    count = 0
+    for i in range(len(nums)-1):
+        if nums[i]>nums[i+1]:
+            count += 1
+            if count > 1:
+                return False 
+        if i > 0 and nums[i-1] > nums[i+1]:
+            nums[i+1] = nums[i]
+        else:
+            nums[i] = nums[i+1]
+    return True
+
+nums = [4, 2, 3]
+print(non_decreasing(nums))
+
+nums = [4, 2, 1]
+print(non_decreasing(nums))
+
+
